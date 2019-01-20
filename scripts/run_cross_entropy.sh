78485294351c800a5714f9e0e055af49e326afc8
hrvojebusic <hrv.busic@gmail.com>
2019-01-20 11:36:41 +0100

    [UPDATE] Cross entropy runner script with center loss

--- scripts/run_cross_entropy.sh
+++ scripts/run_cross_entropy.sh
@@ -1,4 +1,4 @@
 # baseline 
 export save=./results/cross_entropy/${RANDOM}/
 mkdir -p $save
-python ../src/run_cross_entropy.py --dataset=cifar10 --outf $save --dataroot ./data   2>&1 | tee  $save/log.txt
+python ../src/run_cross_entropy.py --centerloss=True --dataset=cifar10 --outf $save --dataroot ./data   2>&1 | tee  $save/log.txt
